26e7e16d7c4e15001fb2c4801a242ebc5819dfd6
nighthawk <73105708+21600429@users.noreply.github.com>
2021-03-19 20:27:28 +0800

    Add files via upload

---
+++ iboxpayCOOKIE.js
@@ -0,0 +1,17 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let refreshtokenVal= `fc9bbbecae7b4e678d3734a42e94ada9
+44dac52fb41e41d9a90a0f7be4a4b01d
+be47df10d06f406495df0d3225e49dd4`
+
+
+let iboxpaycookie = {
+
+  refreshtokenVal: refreshtokenVal,  
+  
+}
+
+module.exports =  iboxpaycookie
+  
+
+
